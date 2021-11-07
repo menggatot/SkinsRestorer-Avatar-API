8952700cffa8ca605a0808a18ec57383396adfde
mbahmodin <email@farrel.xyz>
2021-11-07 15:52:09 +0700

    cache url and image

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import app
 from connection import redis_cache 
-from geturl import GetUrl, IsIn
-from avatar_style.classic import classic
+from player_data import GetUrl, IsIn
+from style import style
 from walrus import *
 
 db = redis_cache()
@@ -10,54 +10,69 @@ from flask import Flask
 app = Flask(__name__)
 
 
-def cache_it(insert_nickname, method, name):
-    if db.get(f'{insert_nickname}_{name}') is None:
+def player_status_cache(nickname, method, name):
+    if db.get(f'{nickname}_{name}') is None:
         print(name, 'Is False' if method is False else 'Is True')
 
         if method is False:
-            db.delete(f'{insert_nickname}_{name}')
-            db.set(f'{insert_nickname}_{name}', 'False', 300)
+            db.delete(f'{nickname}_{name}')
+            db.set(f'{nickname}_{name}', 'False', 300)
         else:
-            db.delete(f'{insert_nickname}_{name}')
-            db.set(f'{insert_nickname}_{name}', 'True', 300)
+            db.delete(f'{nickname}_{name}')
+            db.set(f'{nickname}_{name}', 'True', 300)
 
-def get_avatar(insert_nickname, avatar_size):
-    nickname = IsIn(insert_nickname)  # Name Input
-    nickname_url = GetUrl(insert_nickname)
+def player_url_cache(nickname, the_url):
+    if db.get(f'{nickname}_url') is None:
+        db.delete(f'{nickname}_url')
+        db.set(f'{nickname}_url', the_url, 300)
+        print(f'{nickname}\'s url is cached')
+        return the_url
+    else:
+        print(f'{nickname}\'s url is in cache')
+        return db.get(f'{nickname}_url').decode("utf-8")
 
-    cache_it(insert_nickname, nickname.is_in_db(), 'is_in_db')
-    cache_it(insert_nickname, nickname.is_in_tl(), 'is_in_tl')
-    cache_it(insert_nickname, nickname.is_in_mojang(), 'is_in_mojang')
-    
-    is_in_db = db.get(f'{insert_nickname}_is_in_db').decode("utf-8")
-    is_in_mojang = db.get(f'{insert_nickname}_is_in_mojang').decode("utf-8")
-    is_in_tl = db.get(f'{insert_nickname}_is_in_tl').decode("utf-8")
+def get_avatar(nickname, avatar_size):
+    nickname_status = IsIn(nickname) 
+    nickname_url = GetUrl(nickname)
+
+    player_status_cache(nickname, nickname_status.is_in_db(), 'is_in_db')
+    player_status_cache(nickname, nickname_status.is_in_tl(), 'is_in_tl')
+    player_status_cache(nickname, nickname_status.is_in_mojang(), 'is_in_mojang')
+
+    is_in_db = db.get(f'{nickname}_is_in_db').decode("utf-8")
+    is_in_mojang = db.get(f'{nickname}_is_in_mojang').decode("utf-8")
+    is_in_tl = db.get(f'{nickname}_is_in_tl').decode("utf-8")
     print('db:', is_in_db, 'mjg:', is_in_mojang, 'tl:', is_in_tl)
 
-    # return classic(nickname_url.mojang_head(), avatar_size)
+    # return style(nickname_url.mojang_head(), avatar_size).classic()
 
     if is_in_db == 'False':
-        print(f'{insert_nickname} didn\'t have custom skin', is_in_db)
+        print(f'{nickname} didn\'t have custom skin', is_in_db)
 
         if is_in_mojang == 'True':
-            print(f'{insert_nickname} is premium user', is_in_mojang)
-            return classic(nickname_url.mojang_head(), avatar_size)
+            print(f'{nickname} is premium user', is_in_mojang)
+            head_url = player_url_cache(nickname, nickname_url.mojang_head())
+            return style(head_url, avatar_size).classic()
 
         else:
-            print(f'{insert_nickname} is not premium user', is_in_mojang)
+            print(f'{nickname} is not premium user', is_in_mojang)
 
             if is_in_tl == 'False':
                 print(
-                    f'can\'t find {insert_nickname} in TL so now we use Mojang', is_in_tl)
-                return classic(nickname_url.mojang_head(), avatar_size)
+                    f'can\'t find {nickname} in TL so now we use Mojang', is_in_tl)
+                head_url = player_url_cache(
+                    nickname, nickname_url.mojang_head())
+                return style(head_url, avatar_size).classic()
 
             else:
-                print(f'found {insert_nickname} in TL', is_in_tl)
-                return classic(nickname_url.tl_head(), avatar_size)
+                print(f'found {nickname} in TL', is_in_tl)
+                head_url = player_url_cache(nickname, nickname_url.tl_head())
+                return style(head_url, avatar_size).classic()
 
     else:
-        print(f'{insert_nickname} has custom skin', is_in_db)
-        return classic(nickname_url.db_head(), avatar_size)
+        print(f'{nickname} has custom skin', is_in_db)
+        head_url = player_url_cache(nickname, nickname_url.db_head())
+        return style(head_url, avatar_size).classic()
 
 
 
